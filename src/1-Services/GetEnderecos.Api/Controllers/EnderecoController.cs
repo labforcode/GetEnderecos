e07d7a47dc354123704c1e33498a2b82702ed580
lab4code <lab4code@LAB4CODE>
2020-04-11 18:14:14 -0300

    Criada a classe de controller.

---
+++ src/1-Services/GetEnderecos.Api/Controllers/EnderecoController.cs
@@ -0,0 +1,26 @@
+﻿using GetEnderecos.Application.Interfaces.Enderecos;
+using GetEnderecos.Application.ViewModels.Enderecos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GetEnderecos.Api.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        private readonly IEnderecoService _enderecoService;
+
+        public EnderecoController(IEnderecoService enderecoService)
+        {
+            _enderecoService = enderecoService;
+        }
+
+
+        [HttpGet]
+        [Route("enderecos")]
+        public async Task<EnderecoViewModel> ObterEnderecos()
+        {
+
+            return null;
+        }
+    }
+}
